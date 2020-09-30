a9501ad67575ffe55b391eb5203b0d6a249dc09c
Opilane TTHK <opilane@tthk.ee>
2020-09-30 11:27:18 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express =require('express');
+const ejs=require('ejs');
+const bodyParser = require('body-parser');
+const axios=require('axios');
+const app=express();
+
+
+app.set('view engine',ejs);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render('index.ejs', { data : "" });
+ });
+
+app.post('/',(req, res)=>{
+    let country=req.body.country;
+    let url=`https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+    axios.get(url).
+    then(function(response){
+        console.log(response.data);
+        
+    }).
+    catch(function(error){
+        console.log(error);
+        
+    });      
+        
+
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is running on Port 3000');
+    });
