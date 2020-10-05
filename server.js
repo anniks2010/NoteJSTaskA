51c9286677337412d0ad90b6cc0812e7bd921da4
anniks2010 <anulember@hot.ee>
2020-10-06 00:59:48 +0300

    second

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ app.set('view engine',ejs);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+
 app.get("/", function(req, res){
-    res.render('index.ejs', { data : "" });
+    
+    res.render('index.ejs', {country: ''});
  });
 
 app.post('/',(req, res)=>{
     let country=req.body.country;
     let url=`https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+    console.log(country);
+    
     axios.get(url).
     then(function(response){
-        console.log(response.data);
+        console.log(response.country);
+        let countryObjects=response.data[0];
+        res.render('index.ejs',{country:countryObjects});
+
         
     }).
     catch(function(error){
